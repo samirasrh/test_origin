2b8360e388548aae290e716a2117831030bc95e4
samirasrh <Samira.Briongos.extern@srh-hochschulen.de>
2024-11-22 09:38:24 +0100

    Update Phisable.sol

--- Phisable.sol
+++ Phisable.sol
@@ -1,1 +1,19 @@
+pragma solidity ^0.6.4;
 
+//Example from the Mastering Ethereum book
+// Version of Solidity compiler this program was written for
+
+contract Phishable {
+    address public owner;
+
+    constructor (address _owner) {
+        owner = _owner;
+    }
+
+    function () external payable {} // collect ether
+
+    function withdrawAll(address _recipient) public {
+        require(tx.origin == owner);
+        _recipient.transfer(this.balance);
+    }
+}
